c964ffecd7550dd7fb9c68e7ed593946ca2b4242
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-06-10 19:03:13 -0400

    adding changes to the fetch in JS.

--- styles/app.js
+++ styles/app.js
@@ -1,10 +1,19 @@
-const button = document.getElementById('search')
-const metric = '&units=metric'
-const imperial = '&units=imperial'
-const ForC = metricorimperial
-const input = document.getElementById('inputValue') 
+const button = document.getElementById('search');
+// const metric = '&units=metric';
+// const imperial = '&units=imperial';
+// const ForC = metricorimperial;
+const input = document.getElementById('inputValue').value;
 
+const APIKey = 
 
-button.addEventListener('click', function(
-    _fetch('api.openweathermap.org/data/2.5/forecast/daily?zip={zip code},us&cnt=6&appid='+ APIKey + ForC )
-);
+const Geocoding_API ="api.openweathermap.org/data/2.5/forecast/daily?zip={zip code},{country code}&appid={API key}"
+
+function fetchdata() {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?zip=03060,US&cnt=40&appid=")
+    .then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data)
+    });
+}
+fetchdata();
