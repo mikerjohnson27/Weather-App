6f91a4f5d4422653ad68a76fe2ff4d3e9b1c9e87
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-06-15 16:27:56 -0400

    adding dotenv to file to mask my API key.

--- styles/app.js
+++ styles/app.js
@@ -1,4 +1,4 @@
-//
+
 const button = document.getElementById('search');
 // const metric = '&units=metric';
 // const imperial = '&units=imperial';
@@ -6,15 +6,13 @@ const button = document.getElementById('search');
 //
 const input = document.getElementById('inputValue');
 
-//
 const topCityEl = document.getElementById('App');
 
-//
-const URLs = "https://api.openweathermap.org/data/2.5/forecast?zip="
-const URL2 = ",US&cnt=1&appid="
+const URLs = "api.openweathermap.org/data/2.5/forecast/daily?zip="
+const URL2 = ",US&cnt=36&appid="
 
-//
-const APIKey = "e6eec167479d0b4471b3a49b163e721a"
+const WeatherAPIKey = require('../.env')
+const APIKey = WeatherAPIKey
 
 //
 button.addEventListener("click", fetchData)
@@ -33,56 +31,15 @@ function fetchData() {
     </section>
     <section class="grid-container custom-margin-top">
         <div class="grid-x grid-margin-x align-middle">
-            <img class="cell small-4 align-middle" src="../Weather-App/styles/img/5a29cbf3bba5a5.9531327415126886277686.png" alt="">
+            <img class="cell small-4 align-middle" src="http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png" alt="">
             <div class="cell small-4 align-middle">
             <h2>${data.list[0].main.temp} °F</h2>
-            <h2>${data.list[0].weather.description}</h2>
+            <h2>${data.list[0].weather[0].description}</h2>
             </div>
             <ul class="cell small-4 align-middle">
                 <li>Feels Like: ${data.list[0].main.feels_like} °F</li>
                 <li>Humidity: ${data.list[0].main.humidity}%</li>
             </ul>
-        </div>
-    </section>
-    <section class="grid-container">
-        <div class="grid-y grid-margin-y">
-            <h3 class="text-center">Hourly</h3>
-        </div>
-    </section>
-    <section class="grid-container">
-        <div class="grid-x grid-margin-x custom-margin-top">
-            <div class="cell small-4 timebackground">
-                <img class="cell" src="../Weather-App/styles/img/5a29cbf3bba5a5.9531327415126886277686.png" alt="">
-                <h4 class="cell small-4 text-center">85 °F</h4>
-                <h6 class="cell small-4 text-center">9:00 am</h6>
-            </div>
-            <div class="cell small-4 timebackground">
-                <img class="cell small-4" src="../Weather-App/styles/img/5a29cbf3bba5a5.9531327415126886277686.png" alt="">
-                <h4 class="cell small-4 text-center">85 °F</h4>
-                <h6 class="cell small-4 text-center">11:00 am</h6>
-            </div>
-            <div class="cell small-4 timebackground">
-                <img class="cell small-4" src="../Weather-App/styles/img/5a29cbf3bba5a5.9531327415126886277686.png" alt="">
-                <h4 class="cell small-4 text-center">85 °F</h4>
-                <h6 class="cell small-4 text-center">1:00 pm</h6>
-            </div>
-        </div>
-        <div class="grid-x grid-margin-x custom-margin-top">
-            <div class="cell small-4 timebackground">
-                <img class="cell small-4" src="../Weather-App/styles/img/5a29cbf3bba5a5.9531327415126886277686.png" alt="">
-                <h4 class="cell small-4 text-center">85 °F</h4>
-                <h6 class="cell small-4 text-center">3:00 pm</h6>
-            </div>
-            <div class="cell small-4 timebackground">
-                <img class="cell small-4" src="../Weather-App/styles/img/5a29cbf3bba5a5.9531327415126886277686.png" alt="">
-                <h4 class="cell small-4 text-center">85 °F</h4>
-                <h6 class="cell small-4 text-center">5:00 pm</h6>
-            </div>
-            <div class="cell small-4 timebackground">
-                <img class="cell small-4" src="../Weather-App/styles/img/5a29cbf3bba5a5.9531327415126886277686.png" alt="">
-                <h4 class="cell small-4 text-center">85 °F</h4>
-                <h6 class="cell small-4 text-center">9:00 pm</h6>
-            </div>
         </div>
     </section>
     <section class="grid-container">
@@ -93,30 +50,30 @@ function fetchData() {
     <section class="grid-container">
         <div class="grid-x grid-margin-x custom-margin-top">
             <div class="cell small-4 timebackground">
-                <img class="cell small-4" src="../Weather-App/styles/img/5a29cbf3bba5a5.9531327415126886277686.png" alt="">
-                <h4 class="cell small-4 text-center">85 °F/</h4>
-                <h6 class="cell small-4 text-center">Friday</h6>
+                <img class="cell small-4" src="http://openweathermap.org/img/wn/${data.list[8].weather[0].icon}.png" alt="">
+                <h4 class="cell small-4 text-center">${data.list[8].main.temp} °F/</h4>
+                <h6 class="cell small-4 text-center">Tomorrow</h6>
             </div>
             <div class="cell small-4 timebackground">
-                <img class="cell small-4" src="../Weather-App/styles/img/5a29cbf3bba5a5.9531327415126886277686.png" alt="">
-                <h4 class="cell small-4 text-center">85 °F/</h4>
+                <img class="cell small-4" src="http://openweathermap.org/img/wn/${data.list[16].weather[0].icon}.png" alt="">
+                <h4 class="cell small-4 text-center">${data.list[16].main.temp} °F/</h4>
                 <h6 class="cell small-4 text-center">Saturday</h6>
             </div>
             <div class="cell small-4 timebackground">
-                <img class="cell small-4" src="../Weather-App/styles/img/5a29cbf3bba5a5.9531327415126886277686.png" alt="">
-                <h4 class="cell small-4 text-center">85 °F/</h4>
+                <img class="cell small-4" src="http://openweathermap.org/img/wn/${data.list[24].weather[0].icon}.png" alt="">
+                <h4 class="cell small-4 text-center">${data.list[24].main.temp} °F/</h4>
                 <h6 class="cell small-4 text-center">Sunday</h6>
             </div>
         </div>
         <div class="grid-x grid-margin-x custom-margin-top align-center">
             <div class="cell small-4 timebackground">
-                <img class="cell small-4" src="../Weather-App/styles/img/5a29cbf3bba5a5.9531327415126886277686.png" alt="">
-                <h4 class="cell small-4 text-center">85 °F/</h4>
+                <img class="cell small-4" src="http://openweathermap.org/img/wn/${data.list[32].weather[0].icon}.png" alt="">
+                <h4 class="cell small-4 text-center">${data.list[32].main.temp} °F/</h4>
                 <h6 class="cell small-4 text-center">Monday</h6>
             </div>
             <div class="cell small-4 timebackground">
-                <img class="cell small-4" src="../Weather-App/styles/img/5a29cbf3bba5a5.9531327415126886277686.png" alt="">
-                <h4 class="cell small-4 text-center">85 °F/</h4>
+                <img class="cell small-4" src="http://openweathermap.org/img/wn/${data.list[40].weather[0].icon}.png" alt="">
+                <h4 class="cell small-4 text-center">${data.list[40].main.temp} °F/</h4>
                 <h6 class="cell small-4 text-center">Tuesday</h6>
             </div>
         </div>
